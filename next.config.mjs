0a041256273c889d713bc70d054492a899cd3567
skumararya <skumararya.arya4@gmail.com>
2024-07-02 14:35:19 +0530

    second

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+const nextConfig = {};
 
-const nextConfig = {
-    reactStrictMode: true,
-  output: 'export',
-  distDir: 'dist',
-//   trailingSlash: true,
-//   assetPrefix: '.',
-};
+// const nextConfig = {
+//     reactStrictMode: true,
+//   output: 'export',
+//   distDir: 'dist',
+// //   trailingSlash: true,
+// //   assetPrefix: '.',
+// };
 
 
 export default nextConfig;
